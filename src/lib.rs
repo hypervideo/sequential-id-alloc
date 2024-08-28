7c345b803b067fb709ef7716fad685cc1e7d7e0a
Robert Krahn <robert.krahn@gmail.com>
2024-08-28 02:29:37 +0200

    untie SequentialIdAlloc from u8 (#348)

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,75 @@
-use bitvec::prelude::*;
+pub use bitvec::BitArr;
 
 /// A simple sequential id allocator.
 ///
 /// The allocator will allocate new ids sequentially from 0 to u8::MAX. Unlike
 /// other bitmap / slab allocators, freed ids will not be reused right away,
 /// only when the next id pointer wraps around.
-pub struct SequentialIdAlloc<T = u8> {
-    next_ptr: usize,
-    bits: BitArr![for 255, in u8],
-    _output_type: std::marker::PhantomData<T>,
-}
 
-impl<T> Default for SequentialIdAlloc<T> {
-    fn default() -> Self {
-        Self {
-            next_ptr: Default::default(),
-            bits: Default::default(),
-            _output_type: Default::default(),
+#[macro_export]
+macro_rules! sequential_id_alloc {
+    ($ty:ident, $output_ty:ident, $max:expr, $arr_ty:ident) => {
+pub struct $ty<T = $output_ty, A = $crate::BitArr![for $max - 1, in $arr_ty]> {
+            next_ptr: usize,
+            bits: A,
+            _output_type: std::marker::PhantomData<T>,
         }
-    }
-}
 
-impl<T> SequentialIdAlloc<T>
-where
-    T: From<u8> + Into<u8>,
-{
-    const CAPACITY: usize = u8::MAX as usize + 1;
+        impl<T> Default for $ty<T> {
+            fn default() -> Self {
+                Self {
+                    next_ptr: Default::default(),
+                    bits: Default::default(),
+                    _output_type: Default::default(),
+                }
+            }
+        }
 
-    pub fn alloc(&mut self) -> Option<T> {
-        let index = self
-            .bits
-            .iter()
-            .enumerate()
-            .cycle()
-            .skip(self.next_ptr)
-            .take(Self::CAPACITY)
-            .filter_map(|(i, b)| if !b { Some(i) } else { None })
-            .next()?;
+        impl<T> $ty<T>
+        where
+            T: Into<usize>,
+            T: TryFrom<usize>,
+        {
+            pub const fn max() -> usize {
+                $max
+            }
 
-        self.bits.set(index, true);
-        self.next_ptr = index + 1;
+            pub fn alloc(&mut self) -> Option<T> {
+                let index = self
+                    .bits
+                    .into_iter()
+                    .enumerate()
+                    .cycle()
+                    .skip(self.next_ptr)
+                    .take($max)
+                    .filter_map(|(i, b)| if !b { Some(i) } else { None })
+                    .next()?;
 
-        Some(T::from(index as u8))
-    }
+                self.bits.set(index, true);
+                self.next_ptr = index + 1;
 
-    pub fn dealloc(&mut self, id: T) {
-        let id = id.into() as usize;
-        self.bits.set(id, false);
-    }
+                T::try_from(index).ok()
+            }
 
-    pub fn contains(&self, id: T) -> bool {
-        let id = id.into() as usize;
-        self.bits[id]
-    }
+            pub fn dealloc(&mut self, id: T) {
+                let id = id.into();
+                self.bits.set(id, false);
+            }
 
-    pub fn is_full(&self) -> bool {
-        self.bits.count_zeros() == 0
-    }
+            pub fn contains(&self, id: T) -> bool {
+                let id = id.into();
+                self.bits[id]
+            }
 
-    pub fn size(&self) -> usize {
-        self.bits.count_ones()
-    }
+            pub fn is_full(&self) -> bool {
+                self.bits.count_zeros() == 0
+            }
 
-    #[cfg(debug_assertions)]
-    pub fn debug(&self) {
-        let free = self
-            .bits
-            .iter()
-            .enumerate()
-            .cycle()
-            .skip(self.next_ptr)
-            .take(Self::CAPACITY)
-            .map(|(i, b)| (i, *b))
-            .collect::<Vec<_>>();
-        dbg!(free);
-    }
+            pub fn size(&self) -> usize {
+                self.bits.count_ones()
+            }
+        }
+    };
 }
 
 #[cfg(test)]
@@ -82,14 +77,16 @@ mod tests {
     use super::*;
     use proptest::prelude::*;
 
+    sequential_id_alloc!(SequentialIdAllocU8, u8, 256, u8);
+
     #[test]
     fn test_sequential_alloc_1() {
-        let mut ids = SequentialIdAlloc::default();
+        let mut ids = SequentialIdAllocU8::default();
 
-        assert!(!ids.contains(0));
-        assert_eq!(ids.alloc(), Some(0));
-        assert!(ids.contains(0));
-        assert_eq!(ids.size(), 1);
+        assert!(!ids.contains(0u8));
+        assert_eq!(ids.alloc(), Some(0u8));
+        assert!(ids.contains(0u8));
+        assert_eq!(ids.size(), 1usize);
 
         ids.dealloc(0);
         assert_eq!(ids.alloc(), Some(1));
@@ -98,9 +95,9 @@ mod tests {
 
     #[test]
     fn test_sequential_alloc_2() {
-        let mut ids = SequentialIdAlloc::default();
+        let mut ids = SequentialIdAllocU8::default();
 
-        assert_eq!(ids.alloc(), Some(0));
+        assert_eq!(ids.alloc(), Some(0u8));
         assert_eq!(ids.size(), 1);
 
         assert_eq!(ids.alloc(), Some(1));
@@ -113,8 +110,8 @@ mod tests {
 
     #[test]
     fn test_wrap() {
-        let mut ids = SequentialIdAlloc::<u8>::default();
-        for _ in 0..SequentialIdAlloc::<u8>::CAPACITY {
+        let mut ids = SequentialIdAllocU8::<u8>::default();
+        for _ in 0..SequentialIdAllocU8::<u8>::max() {
             assert!(ids.alloc().is_some());
         }
 
@@ -128,7 +125,7 @@ mod tests {
 
     #[test]
     fn test_arbitrary_inputs() {
-        let alloc = std::cell::RefCell::new(SequentialIdAlloc::default());
+        let alloc = std::cell::RefCell::new(SequentialIdAllocU8::default());
         proptest!(ProptestConfig::with_cases(1000), |(n in 0..u8::MAX, allocate in proptest::bool::weighted(0.8))| {
             let mut alloc = alloc.borrow_mut();
             let size = alloc.size();
